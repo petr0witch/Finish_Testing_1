de3bd7f59c7098783f94f2190442ea3236a7a8ef
Petro_witch <wonkewood@gmail.com>
2023-02-15 16:57:17 +0300

    Write function

--- Итог/Program.cs
+++ Итог/Program.cs
@@ -4,3 +4,21 @@ string[] a = {"hello", "2", "world", ":-)"};
 string[] b = {"1234", "1567", "-2", "computer science"};
 string[] c = {"Russia", "Denmark", "Kazan"};
 
+string[] GetBackArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < string[3]) return array; 
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+PrintArray(GetBackArray(a));
