280553b7621ab1dd69c3b9ac44f73757144af084
Petro_witch <wonkewood@gmail.com>
2023-02-15 16:50:05 +0300

    strings

--- Итог/Program.cs
+++ Итог/Program.cs
@@ -1,1 +1,6 @@
 ﻿//Промежуточное тестирование
+
+string[] a = {"hello", "2", "world", ":-)"};
+string[] b = {"1234", "1567", "-2", "computer science"};
+string[] c = {"Russia", "Denmark", "Kazan"};
+
