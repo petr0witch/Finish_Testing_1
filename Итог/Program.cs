e3f182c64ba056fc46bfa887c07830f4f7407198
Petro_witch <wonkewood@gmail.com>
2023-02-15 17:12:22 +0300

    Update functions

--- Итог/Program.cs
+++ Итог/Program.cs
@@ -5,20 +5,22 @@ string[] b = {"1234", "1567", "-2", "computer science"};
 string[] c = {"Russia", "Denmark", "Kazan"};
 
 string[] GetBackArray(string[] array)
-{
-    for(int i = 0; i < array.Length; i++)
-    {
-        if(array[i] < string[3]) return array; 
-    }
-    return array;
+{   
+    string[] NewArray = new string[array.Length];
+    for(int i = 0, j = 0; i < array.Length; i++) 
+        
+        if(array[i].Length <= 3) 
+        {
+            NewArray[j] = array[i];
+            j++;
+        }
+    return NewArray; 
 }
 
 void PrintArray(string[] array)
 {
     for(int i = 0; i < array.Length; i++)
-    {
         Console.Write($"{array[i]} ");
-    }
 }
 
 PrintArray(GetBackArray(a));
